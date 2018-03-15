49f078dd5fe2e45b211d57a8ca7e414769db86bb
A.L <avishai2345@gmail.com>
2018-03-15 16:52:48 +0200

    add empty acticities for instruments and genre

---
+++ app/src/main/java/com/example/android/musicalbandify/activities/GenreActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.musicalbandify.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.musicalbandify.R;
+
+public class GenreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genre);
+    }
+}
